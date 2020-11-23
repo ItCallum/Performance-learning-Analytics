87b77717e9d9b4037bd59ad61c94ef82be5f570e
Callum Simpson <36512996+ItCallum@users.noreply.github.com>
2020-11-23 22:39:20 +0000

    Add files via upload
    The following has been included 
    - an update to 01-AR.r that includes code that cleans up my dataset more to make it tidy/create extra variables to work with.
    
    CSC8631_202021_Understanding is me getting to understand the data 
    Exploring_leaving_single is an rmd document that contains my exploratory analysis of the single columns, what I have learned from them, how I used what I learnt to clean the database, a few ideas on why people are leaving that I want to try and figure out and possible next step that I want to look.

--- Project/CSC8631_202021/munge/01-A.R
+++ Project/CSC8631_202021/munge/01-A.R
@@ -1,1 +1,40 @@
-# Example preprocessing script.
+cyber_security_4_leaving_survey_responses <- cyber_security_4_leaving_survey_responses %>% mutate(Cycle = 4)
+cyber_security_5_leaving_survey_responses <- cyber_security_5_leaving_survey_responses %>% mutate(Cycle = 5)
+cyber_security_6_leaving_survey_responses <- cyber_security_6_leaving_survey_responses %>% mutate(Cycle = 6)
+cyber_security_7_leaving_survey_responses <- cyber_security_7_leaving_survey_responses %>% mutate(Cycle = 7)
+
+combine_Leaving <- do.call("rbind", list((cyber_security_4_leaving_survey_responses),(cyber_security_5_leaving_survey_responses),
+                      (cyber_security_6_leaving_survey_responses),(cyber_security_7_leaving_survey_responses) ))
+
+levels(combine_Leaving$leaving_reason) <- c(levels(combine_Leaving$leaving_reason), "I do not have enough time")
+levels(combine_Leaving$leaving_reason) <- c(levels(combine_Leaving$leaving_reason), "The course was not what I expected")
+levels(combine_Leaving$leaving_reason) <- c(levels(combine_Leaving$leaving_reason), "The course would not help me reach my goals")
+
+combine_Leaving$leaving_reason[combine_Leaving$leaving_reason == 'I donâ\200\231t have enough time'] <- 'I do not have enough time'
+combine_Leaving$leaving_reason[combine_Leaving$leaving_reason == 'The course wasnâ\200\231t what I expected'] <- 'The course was not what I expected'
+combine_Leaving$leaving_reason[combine_Leaving$leaving_reason == 'The course wonâ\200\231t help me reach my goals'] <- 'The course would not help me reach my goals'
+
+
+combine_Leaving$Cycle <- factor(combine_Leaving$Cycle)
+
+combine_Leaving_NoDups <- combine_Leaving %>% group_by(learner_id,Cycle) %>% filter(n()==1)
+
+combine_Leaving_clean <- combine_Leaving_NoDups
+
+combine_Leaving_clean[is.na(combine_Leaving_clean)] <- 0
+
+combine_Leaving_clean <- combine_Leaving_clean %>% mutate(Date = as.Date(left_at))
+#output))
+
+combine_Leaving_clean$left_at <- as.POSIXct(combine_Leaving_clean$left_at,format="%Y-%m-%d %H:%M:%S") 
+
+##combine_Leaving_date <- combine_Leaving_date %>% mutate(Time = format(ymd_hms(left_at), format = c("%H:%M:%S")))
+
+combine_Leaving_clean <- combine_Leaving_clean %>% mutate(Time = strftime(left_at, format="%H"))
+
+
+combine_Leaving_clean$last_completed_week_number <- as.factor(combine_Leaving_clean$last_completed_week_number)
+
+combine_Leaving_clean$last_completed_step_number <- as.factor(combine_Leaving_clean$last_completed_step_number)
+
+combine_Leaving_clean$last_completed_step <- as.factor(combine_Leaving_clean$last_completed_step)
