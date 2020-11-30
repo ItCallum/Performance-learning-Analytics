7833e14b919990d15ed19826868fc8562ea73d55
CallumSimpson <1234callumsimpson@gmail.com>
2020-11-30 01:26:48 +0000

    Took a while but i figured out how to get my analysis reports into the report section of the project
    - moved the understanding data and cleaning data rmd into the reports folder. Put code into the src

---
+++ Project/CSC8631_202021/src/SC_for_clean_leaving_data.R
@@ -0,0 +1,99 @@
+##The following is the code for the cleaning and anlysis of the 
+
+library("ProjectTemplate")
+load.project()
+
+## ID variables 
+
+table_dup_ID <- table(duplicated(combine_Leaving$id,fromLast=TRUE))
+
+table_dup_Learner_ID <- table(duplicated(combine_Leaving$learner_id,fromLast=TRUE))
+
+
+leavers_duplicated_learner_id <- combine_Leaving %>% group_by(learner_id) %>% filter(n() != 1) 
+
+leavers_duplicated_learner_id[order(leavers_duplicated_learner_id$learner_id),] 
+
+dup_learner_id_and_cycle <-  leavers_duplicated_learner_id  %>% group_by(learner_id,Cycle) %>% filter(n() != 1)
+
+
+removed_duplicate_df <- combine_Leaving %>% 
+  group_by(learner_id,Cycle) %>% 
+  filter(n()==1)
+
+## how_many_people_left 
+
+bar_chart_of_user_per_cycle <- ggplot(combine_Leaving_NoDups, aes(Cycle)) + geom_bar() +
+  geom_text(stat='count', aes(label=..count..), vjust=1)  + ggtitle("How many people left per cycle")
+
+
+reasons_precenatge_df <- as.data.frame(table(combine_Leaving_NoDups$leaving_reason)) %>% mutate(percent = (Freq/sum(Freq)) * 100 ) 
+
+## Reason for leaving
+
+Reason_Prectange <- ggplot(reasons_precenatge_df, aes(x="", y = percent, fill=Var1)) + 
+  geom_bar(width = 1, stat = "identity") + ggtitle("The reason why people left")
+
+Reason_Count <- ggplot(combine_Leaving_NoDups, aes(leaving_reason)) + geom_bar() + 
+  coord_flip() + ggtitle("The reasons why people left")
+
+Reason_Count_cycle <- ggplot(combine_Leaving_NoDups, aes(leaving_reason)) + geom_bar() + facet_wrap(~ Cycle, ncol = 2) + 
+  coord_flip()  + ggtitle("The reasons why people left per cycle")
+
+## Did anyone have a last completed week less than there last complteded step.
+
+Anyone_comp_week_less_than_last_step <- na.omit(combine_Leaving_NoDups) %>% filter(last_completed_week_number < last_completed_week_number) 
+
+## why some many NA
+
+combine_Leaving_NoDups_Zero <- combine_Leaving_NoDups
+
+combine_Leaving_NoDups_Zero[is.na(combine_Leaving_NoDups)] <- 0
+
+combine_Leaving_NoDups_Zero
+
+Na_row_precenatage <- (204 / nrow(combine_Leaving_NoDups_Zero)) * 100 
+
+lcw_table <- table(combine_Leaving_clean$last_completed_week_number)
+
+Leavers_perweek_percycle <- ggplot(combine_Leaving_clean, aes(last_completed_week_number)) + geom_bar() + facet_wrap(~ Cycle, ncol = 2) + ggtitle("The amount of leavers last completed week")
+
+lsc_table <- table(combine_Leaving_clean$last_completed_step)
+
+combine_Leaving_clean_Remove_Zero <- combine_Leaving_clean %>% filter(last_completed_step_number != 0)
+
+no_zero_lcs_bar <- ggplot(combine_Leaving_clean_Remove_Zero, aes(last_completed_step)) + geom_bar() + ggtitle("The amount of leavers last completed step")
+no_zero_lcs_bar_cycle <-ggplot(combine_Leaving_clean_Remove_Zero, aes(last_completed_step)) + geom_bar() + facet_wrap(~ Cycle, ncol = 2) + ggtitle("The amount of leavers last completed step per cycle")
+
+## How many people left 
+
+
+Check_Na <- combine_Leaving_NoDups[rowSums(is.na(combine_Leaving_NoDups)) > 0,]
+
+Check_Na
+
+NA_Leaving_reason <- table(Check_Na$leaving_reason)
+
+Check_Na_leaving_reason_bar <- ggplot(Check_Na, aes(leaving_reason)) + geom_bar() + 
+  coord_flip() + ggtitle("The reason why people left but didnt complete a step")
+
+## The day people left
+
+timeline_Cycle_4 <- combine_Leaving_clean %>% filter(Cycle == 4) %>%  ggplot(aes(x=Date, y=Time, color=last_completed_week_number)) + geom_point() +  geom_vline(xintercept = as.Date("2017-11-13"))
+timeline_Cycle_5 <- combine_Leaving_clean %>% filter(Cycle == 5) %>%  ggplot(aes(x=Date, y=Time, color=last_completed_week_number)) + geom_point() +  geom_vline(xintercept = as.Date("2018-02-05"))
+timeline_Cycle_6 <- combine_Leaving_clean %>% filter(Cycle == 6) %>%  ggplot(aes(x=Date, y=Time, color=last_completed_week_number)) + geom_point() +  geom_vline(xintercept = as.Date("2018-06-11"))
+timeline_Cycle_7 <- combine_Leaving_clean %>% filter(Cycle == 7) %>%  ggplot(aes(x=Date, y=Time, color=last_completed_week_number)) + geom_point() + geom_vline(xintercept = as.Date("2018-09-10"))
+
+
+timeline_full <- ggplot(combine_Leaving_clean, aes(x=Date, y=Time, color=last_completed_week_number),  yaxt = "n") + geom_point()+ facet_wrap(~ Cycle, ncol = 1) + theme(legend.position="bottom")+
+  theme(axis.text.y=element_blank(),axis.ticks.y=element_blank()) + ggtitle("Time line of users leaving and there Last completed week") +  geom_vline(xintercept = as.Date("2017-11-13")) +  geom_vline(xintercept = as.Date("2018-02-05")) +  geom_vline(xintercept = as.Date("2018-06-11")) + geom_vline(xintercept = as.Date("2018-09-10"))
+
+## Whats going on with the person who left at week 3 before the course even started
+
+before_course <- combine_Leaving_clean %>% filter(Cycle == 6 & Date < as.Date("2018-06-11") )
+
+one_person <- combine_Leaving_clean %>% filter(learner_id == "353eaf1b-cff8-4d9c-98d4-b9a736c17e73") 
+
+## "impossible" leavers 
+
+impos_leaver <- combine_Leaving_clean %>% filter(left_at < last_completed_step_at )
